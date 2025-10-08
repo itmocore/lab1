aae53c60cdb3e8ce002706c3b1342a27f88dcb0e
502657 <fedyahonest@gmail.com>
2025-10-08 17:34:10 +0300

    pep8

--- src/main.py
+++ src/main.py
@@ -5,17 +5,19 @@ END = '\u001b[0m'
 h, w = 20, 30
 cx, cy = w // 2, h // 2
 r = 6
-#ур.окр
-def f(x, y):
+
+
+def circle(x, y):
     return (x - cx)**2 + (y - cy)**2 <= r**2
 
 for y in range(h):
     for x in range(w):
-        if f(x, y):
+        if circle(x, y):
             print(f"{RED}  {END}", end="")
         else:
             print(f"{WHITE}  {END}", end="")
     print()
+
 print("task2")
 B = '\u001b[40m'
 W = WHITE
@@ -33,14 +35,16 @@ for y in range(h):
         else:
             print(f"{W}  {E}", end="")
     print()
+
 print('task3')
 for y in range(h, -1, -1):
         for x in range(w + 1):
-            if 3*x== y:
+            if x*3== y:
                 print(f"{RED} {END}", end="")
             else:
                 print(f"{WHITE} {END}", end="")
         print()
-true=sum(float(x)<-5 for x in open('sequence.txt'))
-len=sum(float(x)<=0 for x in open('sequence.txt'))
-print(f'{true*100/len}%[{RED}{' '*true}{END}{'-'*(len-true)}]')
+
+true = sum(float(x)<-5 for x in open('sequence.txt'))
+ln=sum(float(x)<=0 for x in open('sequence.txt'))
+print(f'{true*100/ln}%[{RED}{' '*true}{END}{'-'*(ln-true)}]')
